a9e94b4c578b2f2f41f9eb0ce90ab0708cfa4f45
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-02-23 15:34:10 +0200

    Get extended partition info (#58)
    * Get extended partition info
    
    * Use lsblk instead of parted to read sector size
    
    * Return free parts of disk

--- pyclient/g8core/client.py
+++ pyclient/g8core/client.py
@@ -484,6 +484,11 @@ class DiskManager:
         'part_type': typchk.Enum('primary', 'logical', 'extended'),
     })
 
+    _getpart_chk = typchk.Checker({
+        'disk': str,
+        'part': str,
+    })
+
     _rmpart_chk = typchk.Checker({
         'disk': str,
         'number': int,
@@ -542,10 +547,41 @@ class DiskManager:
         if result.state != 'SUCCESS':
             raise RuntimeError('failed to create table: %s' % result.stderr)
 
+    def getinfo(self, disk, part=''):
+        """
+        Get more info about a disk or a disk partition
+
+        :param disk: (sda, sdb, etc..)
+        :param part: (sda1, sdb2, etc...)
+        :return: a dict with {"blocksize", "start", "size", and "free" sections}
+        """
+        args = {
+            "disk": disk,
+            "part": part,
+        }
+
+        self._getpart_chk.check(args)
+
+        response = self._client.raw('disk.getinfo', args)
+
+        result = response.get()
+
+        if result.state != 'SUCCESS':
+            raise RuntimeError('failed to get info: %s' % result.stderr)
+
+        if result.level != 20:  # 20 is JSON output.
+            raise RuntimeError('invalid response type from disk.getinfo command')
+
+        data = result.data.strip()
+        if data:
+            return json.loads(data)
+        else:
+            return {}
+
     def mkpart(self, disk, start, end, part_type='primary'):
         """
         Make partition on disk
-        :param disk: Full device path like /dev/sda
+        :param disk: device name (sda, sdb, etc...)
         :param start: partition start as accepted by parted mkpart
         :param end: partition end as accepted by parted mkpart
         :param part_type: partition type as accepted by parted mkpart
@@ -569,7 +605,7 @@ class DiskManager:
     def rmpart(self, disk, number):
         """
         Remove partion from disk
-        :param disk: Full device path like /dev/sda
+        :param disk: device name (sda, sdb, etc...)
         :param number: Partition number (starting from 1)
         """
         args = {
