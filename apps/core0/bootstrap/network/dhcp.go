a46b34f9232a414895599b3e9e7718e36c9fd2c0
Maxime Daniel <maxux.unix@gmail.com>
2018-04-01 09:15:14 +0200

    Set hostname during DHCP request (#595)
    * Add zerotier id on dhcp request, set hostname on dhcp request
    
    * Fix dhcp hostname syntax

--- apps/core0/bootstrap/network/dhcp.go
+++ apps/core0/bootstrap/network/dhcp.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/pborman/uuid"
 	"github.com/zero-os/0-core/base/pm"
+	"io/ioutil"
 )
 
 const (
@@ -17,7 +18,23 @@ func init() {
 type dhcpProtocol struct {
 }
 
+func (d *dhcpProtocol) getZerotierId() (string, error) {
+	bytes, err := ioutil.ReadFile("/tmp/zt/identity.public")
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes)[0:10], nil
+}
+
 func (d *dhcpProtocol) Configure(mgr NetworkManager, inf string) error {
+	hostid := "hostname:zero-os"
+
+	ztid, err := d.getZerotierId()
+	if err == nil {
+		hostid = fmt.Sprintf("hostname:zero-os-%s", ztid)
+	}
+
 	cmd := &pm.Command{
 		ID:      uuid.New(),
 		Command: pm.CommandSystem,
@@ -31,7 +48,9 @@ func (d *dhcpProtocol) Configure(mgr NetworkManager, inf string) error {
 					"-A", "3", //wait 3 seconds between each trial
 					"--now",  //exit if failed after consuming all the trials (otherwise stay alive)
 					"--quit", //quit once the lease is obtained
-					"-s", "/usr/share/udhcp/simple.script"},
+					"-s", "/usr/share/udhcp/simple.script",
+					"-x", hostid, //set hostname on dhcp request
+				},
 			},
 		),
 	}
