ac81c28540a67c9d658f93ef8063bce30efc152f
Muhamad Azmy <muhamada@greenitglobe.com>
2018-08-13 15:26:15 +0200

    Set default hub url

--- apps/core0/subsys/containers/filesystem.go
+++ apps/core0/subsys/containers/filesystem.go
@@ -33,7 +33,7 @@ func (c *container) mountFList(src string, target string, hooks ...pm.RunnerHook
 	namespace := fmt.Sprintf("containers/%s", c.name())
 	storage := c.Args.Storage
 	if storage == "" {
-		storage = settings.Settings.Globals.Get("storage", "ardb://hub.gig.tech:16379")
+		storage = settings.Settings.Globals.Get("storage", "zdb://hub.gig.tf:9900")
 		c.Args.Storage = storage
 	}
 
