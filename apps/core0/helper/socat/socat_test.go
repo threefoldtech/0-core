01787e885e527677a5a68ab2179c292479cd119e
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-08-16 15:04:56 +0200

    refactor socat test to use table testing

--- apps/core0/helper/socat/socat_test.go
+++ apps/core0/helper/socat/socat_test.go
@@ -4,49 +4,43 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestParseSource(t *testing.T) {
-	src, err := getSource("2200")
-	if ok := assert.NoError(t, err); !ok {
-		t.Fatal()
+
+	for _, tc := range []struct {
+		input    string
+		expected source
+	}{
+		{
+			"2200",
+			source{"0.0.0.0", 2200},
+		},
+		{
+			"0.0.0.0:2200",
+			source{"0.0.0.0", 2200},
+		},
+		{
+			"10.20.30.40:2200",
+			source{"10.20.30.40", 2200},
+		},
+		{
+			"eth0:2200",
+			source{"eth0", 2200},
+		},
+	} {
+		t.Run(tc.input, func(t *testing.T) {
+			src, err := getSource(tc.input)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, src)
+		})
 	}
 
-	if ok := assert.Equal(t, source{"0.0.0.0", 2200}, src); !ok {
-		t.Error()
+	for _, tc := range []string{"eth0:0", "eth0", ""} {
+		t.Run(tc, func(t *testing.T) {
+			_, err := getSource(tc)
+			assert.Error(t, err)
+		})
 	}
-
-	src, err = getSource("10.20.30.40:2200")
-	if ok := assert.NoError(t, err); !ok {
-		t.Fatal()
-	}
-
-	if ok := assert.Equal(t, source{"10.20.30.40", 2200}, src); !ok {
-		t.Error()
-	}
-
-	src, err = getSource("eth0:2200")
-	if ok := assert.NoError(t, err); !ok {
-		t.Fatal()
-	}
-
-	if ok := assert.Equal(t, source{"eth0", 2200}, src); !ok {
-		t.Error()
-	}
-
-	src, err = getSource("eth0:0")
-	if ok := assert.Error(t, err); !ok {
-		t.Fatal()
-	}
-
-	src, err = getSource("eth0")
-	if ok := assert.Error(t, err); !ok {
-		t.Fatal()
-	}
-
-	src, err = getSource("")
-	if ok := assert.Error(t, err); !ok {
-		t.Fatal()
-	}
-
 }
