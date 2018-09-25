66f5d0c75782ced472c061d89e212c5462292200
john-kheir <kheirj@codescalers.com>
2018-09-25 10:02:20 +0200

    add -u to stream the output

--- tests/install_zt_js.sh
+++ tests/install_zt_js.sh
@@ -5,9 +5,6 @@ curl -s https://install.zerotier.com/ | sudo bash
 zerotier-cli join ${ZT_NET_ID}
 memberid=$(sudo zerotier-cli info | awk '{print $3}')
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${TLRE_ZT_TOKEN}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${ZT_NET_ID}/member/${memberid} > /dev/null
-sleep 5
-zerotier-cli listnetworks
-ping 10.147.20.231 -c 5
 
 set -e
 sudo ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
