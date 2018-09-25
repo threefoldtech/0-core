e0aba00dbdd3a4267e70196ce6e8a802b684e07d
john-kheir <kheirj@codescalers.com>
2018-09-25 08:15:41 +0200

    fix

--- tests/install_zt_js.sh
+++ tests/install_zt_js.sh
@@ -2,9 +2,9 @@
 
 # install zerotier and join the network
 curl -s https://install.zerotier.com/ | sudo bash
-zerotier-cli join ${TLRE_ZT_TOKEN}
+zerotier-cli join ${ZT_NET_ID}
 memberid=$(sudo zerotier-cli info | awk '{print $3}')
-curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${2}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${1}/member/${memberid} > /dev/null
+curl -s -H "Content-Type: application/json" -H "Authorization: Bearer ${TLRE_ZT_TOKEN}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${ZT_NET_ID}/member/${memberid} > /dev/null
 sleep 5
 ping 10.147.20.231 -c 5
 
