27792495fc2fc3c8d358c154cdb25a0d2dc53798
john-kheir <kheirj@codescalers.com>
2018-09-26 14:14:13 +0200

    fix5

--- tests/before_install.sh
+++ tests/before_install.sh
@@ -1,3 +1,8 @@
+sudo apt-get install -y python3.5 python3.5-dev
+sudo rm -f /usr/bin/python
+sudo rm -f /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
 export ubuntu_port=$((2500 + RANDOM % 1000))
 export vm_ubuntu_name=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')
 export vm_zos_name=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')
