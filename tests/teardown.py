442168b4de384d64459f30ff2440d3deb13415f0
john-kheir <kheirj@codescalers.com>
2018-10-11 16:20:38 +0200

    teardown zdb and namespace

--- tests/teardown.py
+++ tests/teardown.py
@@ -4,17 +4,30 @@ import os
 
 
 def teardown(options):
+    zos_vm_name = os.environ['vm_zos_name']
     zos_client = j.clients.zos.get('zos-kds-farm', data={'host': '{}'.format(options.zos_ip)})
     vms = zos_client.client.kvm.list()
     vm_uuid = [vm['uuid'] for vm in vms if vm['name'] == os.environ['vm_ubuntu_name']]
     if vm_uuid:
         zos_client.client.kvm.destroy(vm_uuid[0])
-    vm_uuid = [vm['uuid'] for vm in vms if vm['name'] == os.environ['vm_zos_name']]
+    vm_uuid = [vm['uuid'] for vm in vms if vm['name'] == zos_vm_name]
     if vm_uuid:
         zos_client.client.kvm.destroy(vm_uuid[0])
     bridge = os.environ['bridge']
     if bridge in zos_client.client.bridge.list():
         zos_client.client.bridge.delete(bridge)
+    # remove zdb
+    zos_vm = zos_client.client.kvm.get(zos_vm_name)
+    zdb_cont_ip = zos_vm['params']['media'][0]['url'].split('zdb://')[1].split(':')[0]
+    zdbs = zos_client.zerodbs.list()
+    for zdb in zdbs:
+        zdb_ip = str(zdb.container.default_ip()).split('/')[0]
+        if zdb_ip == zdb_cont_ip:
+            zdb.destroy()
+            break
+    # remove namespace
+    zos_client.client.bash('rm -rf /mnt/zdbs/sda/data/mydisk').get()
+
     # leave the zt-network
     os.system('zerotier-cli leave {}'.format(os.environ['ZT_NET_ID']))
 
