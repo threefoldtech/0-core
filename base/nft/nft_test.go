62a1348dddf1ae415f42e7e488d74dbef2df9c2c
Muhamad Azmy <muhamada@greenitglobe.com>
2018-11-15 15:54:59 +0200

    Rewrite nft parsing using json

--- base/nft/nft_test.go
+++ base/nft/nft_test.go
@@ -39,49 +39,7 @@ func TestMarshal(t *testing.T) {
 }
 
 func TestFindRules(t *testing.T) {
-	config := `table ip nat {
-	chain pre {
-		type nat hook prerouting priority 0; policy accept;
-		iif "core0" mark set 0x00000001 # handle 3
-		iif "kvm0" mark set 0x00000001 # handle 6
-		tcp dport 6600 dnat to 172.18.0.2:6600 # handle 9
-		tcp dport 2200 dnat to 172.19.0.2:22 # handle 10
-	}
-
-	chain post {
-		type nat hook postrouting priority 0; policy accept;
-		ip saddr 172.18.0.0/16 masquerade # handle 5
-		ip saddr 172.19.0.0/16 masquerade # handle 7
-	}
-}
-table inet filter {
-	chain input {
-		type filter hook input priority 0; policy drop;
-		ct state { related, established} accept # handle 4
-		iifname "lo" accept # handle 5
-		iifname "vxbackend" accept # handle 6
-		ip protocol 1 accept # handle 7
-		iif "core0" udp dport { 68, 53, 67} accept # handle 8
-		tcp dport 6600 accept # handle 11
-		tcp dport 6379 accept # handle 12
-		tcp dport 22 accept # handle 13
-		iif "kvm0" udp dport { 67, 53, 68} accept # handle 16
-		tcp dport 5900 accept # handle 17
-	}
-
-	chain forward {
-		type filter hook forward priority 0; policy accept;
-		iif "core0" oif "core0" mark set 0x00000002 # handle 9
-		oif "core0" mark 0x00000001 drop # handle 10
-		iif "kvm0" oif "kvm0" mark set 0x00000002 # handle 14
-		oif "kvm0" mark 0x00000001 drop # handle 15
-	}
-
-	chain output {
-		type filter hook output priority 0; policy accept;
-	}
-}
-	`
+	config := `{"nftables": [{"table": {"family": "ip", "name": "nat", "handle": 0}}, {"set": {"family": "ip", "elem": ["10.20.1.1", "172.18.0.1", "172.19.0.1"], "name": "host", "table": "nat", "handle": 0, "type": "ipv4_addr"}}, {"chain": {"hook": "prerouting", "family": "ip", "table": "nat", "prio": 0, "name": "pre", "type": "nat", "handle": 1, "policy": "accept"}}, {"rule": {"chain": "pre", "family": "ip", "table": "nat", "handle": 7, "expr": [{"match": {"left": {"payload": {"field": "daddr", "name": "ip"}}, "right": "@host"}}, {"match": {"left": {"meta": "iifname"}, "right": "eth0"}}, {"match": {"left": {"payload": {"field": "dport", "name": "tcp"}}, "right": 8000}}, {"dnat": {"addr": "172.18.0.2", "port": 7999}}]}}, {"chain": {"hook": "postrouting", "family": "ip", "table": "nat", "prio": 0, "name": "post", "type": "nat", "handle": 2, "policy": "accept"}}, {"rule": {"chain": "post", "family": "ip", "table": "nat", "handle": 4, "expr": [{"match": {"left": {"payload": {"field": "saddr", "name": "ip"}}, "right": {"prefix": {"addr": "172.18.0.0", "len": 16}}}}, {"masquerade": null}]}}, {"rule": {"chain": "post", "family": "ip", "table": "nat", "handle": 5, "expr": [{"match": {"left": {"payload": {"field": "saddr", "name": "ip"}}, "right": {"prefix": {"addr": "172.19.0.0", "len": 16}}}}, {"masquerade": null}]}}, {"table": {"family": "inet", "name": "filter", "handle": 0}}, {"chain": {"hook": "prerouting", "family": "inet", "table": "filter", "prio": 0, "name": "pre", "type": "filter", "handle": 1, "policy": "accept"}}, {"chain": {"hook": "forward", "family": "inet", "table": "filter", "prio": 0, "name": "forward", "type": "filter", "handle": 2, "policy": "accept"}}, {"chain": {"hook": "output", "family": "inet", "table": "filter", "prio": 0, "name": "output", "type": "filter", "handle": 3, "policy": "accept"}}, {"chain": {"hook": "input", "family": "inet", "table": "filter", "prio": 0, "name": "input", "type": "filter", "handle": 4, "policy": "drop"}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 5, "expr": [{"match": {"left": {"ct": {"key": "state"}}, "right": {"set": ["established", "related"]}}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 6, "expr": [{"match": {"left": {"meta": "iifname"}, "right": "lo"}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 7, "expr": [{"match": {"left": {"meta": "iifname"}, "right": "vxbackend"}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 8, "expr": [{"match": {"left": {"payload": {"field": "protocol", "name": "ip"}}, "right": 1}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 9, "expr": [{"match": {"left": {"meta": "iif"}, "right": "core0"}}, {"match": {"left": {"payload": {"field": "dport", "name": "udp"}}, "right": {"set": [53, 67, 68]}}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 10, "expr": [{"match": {"left": {"payload": {"field": "dport", "name": "tcp"}}, "right": 22}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 11, "expr": [{"match": {"left": {"payload": {"field": "dport", "name": "tcp"}}, "right": 6379}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 12, "expr": [{"match": {"left": {"meta": "iif"}, "right": "kvm0"}}, {"match": {"left": {"payload": {"field": "dport", "name": "udp"}}, "right": {"set": [53, 67, 68]}}}, {"accept": null}]}}, {"rule": {"chain": "input", "family": "inet", "table": "filter", "handle": 13, "expr": [{"match": {"left": {"payload": {"field": "dport", "name": "tcp"}}, "right": 5900}}, {"accept": null}]}}]}`
 
 	ruleset, err := Parse(config)
 
@@ -95,7 +53,7 @@ table inet filter {
 			Chains: Chains{
 				"pre": Chain{
 					Rules: []Rule{
-						{Body: "tcp dport 2200 dnat to 172.19.0.2:22"},
+						{Body: "ip daddr @host iifname \"eth0\" tcp dport 8000 dnat to 172.18.0.2:7999"},
 					},
 				},
 			},
@@ -117,11 +75,11 @@ table inet filter {
 		t.Fatal()
 	}
 
-	if ok := assert.Equal(t, 10, sub["nat"].Chains["pre"].Rules[0].Handle); !ok {
+	if ok := assert.Equal(t, 7, sub["nat"].Chains["pre"].Rules[0].Handle); !ok {
 		t.Error()
 	}
 
-	if ok := assert.Equal(t, 17, sub["filter"].Chains["input"].Rules[0].Handle); !ok {
+	if ok := assert.Equal(t, 13, sub["filter"].Chains["input"].Rules[0].Handle); !ok {
 		t.Error()
 	}
 }
