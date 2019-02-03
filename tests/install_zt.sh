3f1dc3e8d3d038c843360e86e6e8e114b440f537
john-kheir <kheirj@codescalers.com>
2019-02-03 14:00:59 +0200

    fix18

--- tests/install_zt.sh
+++ tests/install_zt.sh
@@ -2,7 +2,7 @@
 
 # install zerotier and join the network
 curl -s https://install.zerotier.com/ | sudo bash
-zerotier-cli join ${ZT_NET_ID}
+sudo zerotier-cli join ${ZT_NET_ID}
 # fix for travis - zerotier issue
 sudo ifconfig "$(ls /sys/class/net | grep zt)" mtu 1280
 
