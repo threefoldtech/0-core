6023f909204c2ff5285dc50ced51c762bdea0682
john-kheir <kheirj@greenitglobe.com>
2019-02-04 08:56:58 +0200

    Development tests setup (#119)
    * Removing js dependency + creating qcow disk for zos-vm
    
    * fixes
    
    * fixes2
    
    * remove -e
    
    * fix3
    
    * fix4
    
    * fix5
    
    * fix6
    
    * fix8
    
    * fix9
    
    * fix10
    
    * fix11
    
    * install requests
    
    * fix12
    
    * fix13
    
    * fix14
    
    * fix15
    
    * fix16
    
    * fix
    
    * fix18
    
    * fix19

---
+++ tests/install_zt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install zerotier and join the network
+curl -s https://install.zerotier.com/ | sudo bash
+sudo zerotier-cli join ${ZT_NET_ID}
+# fix for travis - zerotier issue
+sudo ifconfig "$(ls /sys/class/net | grep zt)" mtu 1280
+
+# generate a key
+sudo ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
+export pub_key=$(sudo cat /root/.ssh/id_rsa.pub)
--- tests/teardown.py
+++ tests/teardown.py
@@ -1,43 +1,48 @@
 from argparse import ArgumentParser
-from jumpscale import j
+from zeroos.core0 import client
 import os
+import requests
 
 
 def teardown(options):
     zos_vm_name = os.environ['vm_zos_name']
-    zos_client = j.clients.zos.get('zos-kds-farm', data={'host': '{}'.format(options.zos_ip)})
-    print('Removing namespace and zdb')
-    # remove zdb
-    zos_vm = zos_client.client.kvm.get(zos_vm_name)
-    zdb_cont_ip = zos_vm['params']['media'][0]['url'].split('zdb://')[1].split(':')[0]
-    zdbs = zos_client.zerodbs.list()
-    for zdb in zdbs:
-        zdb_ip = str(zdb.container.default_ip()).split('/')[0]
-        if zdb_ip == zdb_cont_ip:
-            zdb_path = zdb.path
-            zdb.destroy()
-            break
-    # remove namespace
-    zos_client.client.bash('rm -rf {}/data/mydisk'.format(zdb_path)).get()
+    # get zeroos host client
+    params = {'grant_type': 'client_credentials','client_id': options.client_id,
+              'client_secret': options.client_secret,'response_type': 'id_token',
+              'scope': 'user:memberof:threefold.sysadmin,offline_access'}
+    jwt = requests.post('https://itsyou.online/v1/oauth/access_token', params=params).text
+    zos_client = client.Client(options.zos_ip, password=jwt)
+
     # remove vms and bridge
     print('removing vms and bridge')
-    vms = zos_client.client.kvm.list()
+    vms = zos_client.kvm.list()
     vm_uuid = [vm['uuid'] for vm in vms if vm['name'] == os.environ['vm_ubuntu_name']]
     if vm_uuid:
-        zos_client.client.kvm.destroy(vm_uuid[0])
+        zos_client.kvm.destroy(vm_uuid[0])
     vm_uuid = [vm['uuid'] for vm in vms if vm['name'] == zos_vm_name]
     if vm_uuid:
-        zos_client.client.kvm.destroy(vm_uuid[0])
+        zos_client.kvm.destroy(vm_uuid[0])
     bridge = os.environ['bridge']
-    if bridge in zos_client.client.bridge.list():
-        zos_client.client.bridge.delete(bridge)
+    if bridge in zos_client.bridge.list():
+        zos_client.bridge.delete(bridge)
+
+    # remove the zos_vm disk from the host 
+    print('removing zos_vm disk')
+    zos_client.bash('rm -rf /var/cache/{}.qcow2'.format(os.environ['ubuntu_port']))
+
     # leave the zt-network
     os.system('zerotier-cli leave {}'.format(os.environ['ZT_NET_ID']))
+
+
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-z", "--zos_ip", type=str, dest="zos_ip", required=True,
                         help="IP of the zeroos machine that will be used")
+    parser.add_argument("-d", "--client_id", type=str, dest="client_id",
+                        help="client id to generate token for zos client")
+    parser.add_argument("-s", "--client_secret", type=str, dest="client_secret",
+                        help="client secret to generate token for zos client")
     options = parser.parse_args()
     teardown(options)
