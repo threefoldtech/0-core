0993b03a4a8f21483f672962685e7141f1e715f1
Muhamad Azmy <muhamad.azmy+git@gmail.com>
2019-02-14 15:45:55 +0200

    Better socat list

--- apps/plugins/socat/plugin/actions.go
+++ apps/plugins/socat/plugin/actions.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/threefoldtech/0-core/apps/plugins/nft"
 	"github.com/threefoldtech/0-core/base/pm"
@@ -24,27 +25,26 @@ func (s *socatManager) list(ctx pm.Context) (interface{}, error) {
 		return nil, err
 	}
 
-	var rules []string
-	for _, rule := range matches {
-		rules = append(rules, rule.Body)
+	type simpleRule struct {
+		From string `json:"from"`
+		To   string `json:"to"`
 	}
 
-	return rules, nil
-	// return nil
-	// type simpleRule struct {
-	// 	From string `json:"from"`
-	// 	To   string `json:"to"`
-	// }
+	var m []simpleRule
+	for _, r := range matches {
+		rule, err := getRuleFromNFTRule(r.Body)
+		if err != nil {
+			log.Warningf("failed to parse rule: %s", err)
+			continue
+		}
 
-	// m := make(map[int]simpleRule)
-	// for s, r := range s.rules {
-	// 	m[s] = simpleRule{
-	// 		From: fmt.Sprintf("%s:%d", r.source.ip, r.source.port),
-	// 		To:   fmt.Sprintf("%s:%d", r.ip, r.port),
-	// 	}
-	// }
+		m = append(m, simpleRule{
+			From: fmt.Sprintf("%s:%d", rule.source.ip, rule.source.port),
+			To:   fmt.Sprintf("%s:%d", rule.ip, rule.port),
+		})
+	}
 
-	// return m, nil
+	return m, nil
 }
 
 func (s *socatManager) reserve(ctx pm.Context) (interface{}, error) {
